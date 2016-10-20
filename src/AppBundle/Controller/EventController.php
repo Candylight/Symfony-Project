30f3085b74638f0483bc135006c9b012e7e913a7
sgourier <sylvain.gourier@macroprog.com>
2016-10-20 17:21:40 +0200

    Ajout controllers

---
+++ src/AppBundle/Controller/EventController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylva
+ * Date: 20/10/2016
+ * Time: 15:21
+ */
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Event;
+use AppBundle\Entity\User;
+use AppBundle\Form\EventType;
+use Doctrine\Common\Annotations\Annotation\Required;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class EventController extends Controller
+{
+    
+}
