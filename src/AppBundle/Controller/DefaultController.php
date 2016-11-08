6eb4bbde1ebe8818d1e2df6f6731895c72225a3b
Thibault <th.jullion@gmail.com>
2016-11-08 13:42:00 +0100

    [INTEGRATION] Création page Dashboard

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,7 +13,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
@@ -23,8 +22,16 @@ class DefaultController extends Controller
      */
     public function dayeventAction(Request $request)
     {
-        // replace this example code with whatever you need
         return $this->render('default/daymap.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboardAction(Request $request)
+    {
+        return $this->render('default/dashboard.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
     }
---
+++ web/js/dashboard.js
@@ -0,0 +1,6 @@
+
+
+$(".item-dashboard").click( function(){
+   $(".item-dashboard").removeClass("active");
+   $(this).addClass("active");
+});
