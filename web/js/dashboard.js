453ecb820906df656be742e7a085b87316676fb4
Thibault <th.jullion@gmail.com>
2016-11-08 17:05:36 +0100

    [NAVIGATION] Chargement avec Ancre

--- web/js/dashboard.js
+++ web/js/dashboard.js
@@ -1,12 +1,38 @@
 
 
+/*******************************************************************************/
+/*                            CHARGEMENT PAGE                                  */
+/*******************************************************************************/
+if(document.location.hash != null){
+
+   var hash = document.location.hash.replace("#","");
+
+   $(".item-dashboard").each(function(){
+      var page_name = $(this).data("pagename");
+      if( page_name == hash ){
+         var path = $(this).data("path");
+         changeTab(path,page_name);
+      }
+   });
+}
+
+
+
+
+
+
+/*******************************************************************************/
+/*                              INTERACTIONS                                   */
+/*******************************************************************************/
+
 $(".item-dashboard").click( function(){
    $(".item-dashboard").removeClass("active");
    $(this).addClass("active");
-   changeTab($(this).data('path'));
+   changeTab($(this).data('path'),$(this).data('pagename'));
 });
 
-function changeTab(path)
+
+function changeTab(path,page_name)
 {
    $('.loading-icon').addClass("active");
    $('#content-dashboard').html("");
@@ -16,8 +42,20 @@ function changeTab(path)
       method: "POST",
       dataType: "html"
    }).done(function(data) {
+      document.location.hash = page_name;
       $('#content-dashboard').html(data);
       $('.loading-icon').removeClass("active");
    });
 }
+
+
+
+
+
+
+
+
+
+
+
 
